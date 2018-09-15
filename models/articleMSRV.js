e64a780fda56a1e2fb012f0e953e82daea2bc874
xavifserra <38772006+xavifserra@users.noreply.github.com>
2018-09-16 00:03:26 +0200

    new model

--- models/articleMSRV.js
+++ models/articleMSRV.js
@@ -6,30 +6,19 @@ const UserSchema = Users.schema//require('mongoose').model('User').schema;
 const  { ObjectId } = mongoose.SchemaTypes;
 
 const articleSchema = new mongoose.Schema({
-  // idArticle: { type: ObjectId, index: true, required: true, auto: true, },
-  source: { id: String, name: String },
-  author: String,
+  source: { id:String, name: { type:String, default: '' } },
+  author: { type:String, default: '' },
   language: { type: String, default: 'en' },
-  postedBy: UserSchema,
-  title: String,
-  description: String,
-  url: String,
-  urlToImage: String,
+  title: { type:String, default: '' },
+  description: { type:String, default: '' },
+  url: { type:String, default: '' },
+  urlToImage: { type:String, default: '' },
   publishedAt: { type:Date, default: Date.now },
-  starred: Number,
-  likes: Number,
-  dislikes: Number,
-  shared: Number,
-  comments: [{
-    timeStamp:{ type:Date, default:Date.now },
-    user:{ type : ObjectId, ref: 'User' },
-    text: String,
-    rating: Number,
-    likes: Number,
-    dislikes: Number,
-    shared: Number,
-  }],
-
+  favorites: { type:Number, default: 0 },
+  dislikes: { type:Number, default: 0 },
+  shared: { type:Number, default: 0 },
+  postedBy: UserSchema,
+  comments: [{ type: ObjectId, ref: 'Comment' }],
 });
 
 const Article = mongoose.model(process.env.MICROSERVICE_DATABASE, articleSchema);
